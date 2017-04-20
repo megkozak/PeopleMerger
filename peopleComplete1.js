8e054417c8ae9bfb97d845eb97e5548e535579e9
Meg Kozak <megkozak@gmail.com>
2017-04-20 16:05:54 -0400

    sorted into JSON file

--- peopleComplete1.js
+++ peopleComplete1.js
@@ -1,34 +1,41 @@
 var fs = require('fs');
 
-var people1 = people1;
-var people2 = people2;
-var people =
-
+// var people1 = people1;
+// var people2 = people2;
+var people = [];
 
 fs.readFile('people1.json','utf-8', function (err, data) {
     if(err){
       console.log(err);
-    }else {
-      var people1 = (JSON.parse(data)).people1;
-      console.log(data);
+    } else {
+      for (person of JSON.parse(data)) {
+        people.push(person)
+      }
     }
+    fs.readFile('people2.json','utf-8', function (err, data) {
+          if(err){
+            console.log(err);
+          } else {
+            for (person of JSON.parse(data)) {
+              people.push(person)
+            }
+          }
+          console.log(people)
+          people.sort(function (first, second) {
+            return first < second
+          })
 
+          fs.writeFile('peopleComplete.txt', people, 'utf8', function (err, data) {
+              if (err){
+                console.log(err);
+              }
+              console.log(err, data)
+              console.log('people > peopleComplete.txt');
+          });
+    });
 });
 
-fs.readFile('people2.json','utf-8', function (err, data) {
-        if(err){
-          console.log(err);
-        }else {
-          var people2 = (JSON.parse(data)).people2;
-          console.log(data);
-        }
-});
 
-fs.writeFileSynch('peopleComplete.txt', people, 'utf8', callback); {
-    if (err){
-      console.log(err);
-    }
-    return console.log(err);
-    console.log('people > peopleComplete.txt');
-}
+
+
 // + data.toString());
